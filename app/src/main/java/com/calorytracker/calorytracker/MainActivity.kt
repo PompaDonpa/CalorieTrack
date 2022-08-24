caae45e6c51eb57e0b828a0ce6fbe447ca253b1e
Daniel Naranjo <mr.daniel.naranjo@protonmail.com>
2022-08-24 12:01:31 -0400

    Part 3  - Navigation

--- app/src/main/java/com/calorytracker/calorytracker/MainActivity.kt
+++ app/src/main/java/com/calorytracker/calorytracker/MainActivity.kt
@@ -3,15 +3,37 @@ package com.calorytracker.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.calorytracker.onboarding_presentation.WelcomeScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.calorytracker.calorytracker.navigation.navigate
 import com.calorytracker.calorytracker.ui.theme.CaloryTrackerTheme
+import com.calorytracker.core.navigation.Route
+import com.calorytracker.onboarding_presentation.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CaloryTrackerTheme {
-                WelcomeScreen()
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.WELCOME
+                ){
+                    composable(Route.WELCOME){
+                        WelcomeScreen(onNavigate = navController::navigate )
+                    }
+                    composable(Route.AGE){}
+                    composable(Route.GENDER){}
+                    composable(Route.HEIGHT){}
+                    composable(Route.WEIGHT){}
+                    composable(Route.NUTRIENT_GOAL){}
+                    composable(Route.ACTIVITY){}
+                    composable(Route.GOAL){}
+                    composable(Route.TRACKER_OVERVIEW){}
+                    composable(Route.SEARCH){}
+                }
             }
         }
     }
