f77632718bfd174017b02187d8795c924ca2b801
Daniel Naranjo <mr.daniel.naranjo@protonmail.com>
2022-09-11 19:48:46 -0400

    Part 20 - SearchViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/calorytracker/tracker_presentation/search/SearchState.kt
@@ -0,0 +1,10 @@
+package com.calorytracker.tracker_presentation.search
+
+import com.calorytracker.tracker_domain.model.TrackableFood
+
+data class SearchState(
+    val query: String  = "",
+    val isHintVisible: Boolean = false,
+    val isSearching: Boolean = false,
+    val trackableFood: List<TrackableFoodUiState> = emptyList()
+)
---
+++ tracker/tracker_presentation/src/main/java/com/calorytracker/tracker_presentation/search/TrackableFoodUiState.kt
@@ -0,0 +1,9 @@
+package com.calorytracker.tracker_presentation.search
+
+import com.calorytracker.tracker_domain.model.TrackableFood
+
+data class TrackableFoodUiState(
+    val food: TrackableFood,
+    val isExpanded: Boolean = false,
+    val amount: String = ""
+)
